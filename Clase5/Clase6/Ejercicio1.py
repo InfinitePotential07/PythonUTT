2666645fe504a85af1525098963b3e395fc85b8d
InfinitePotential07 <ulisesteran2001@gmail.com>
2026-06-08 09:42:20 -0700

    Nuevo Ejercicio

---
+++ Clase5/Clase6/Ejercicio1.py
@@ -0,0 +1,16 @@
+#Acumulador condicionado con reinicio. Al insertar por el usuario 10 valores, si es numero positivo se sumará,
+#si es numero negativo se restará y si es 0 se va a reiniciar. 
+#Al final se va a mostrar el valor final del acumulador.
+print("Ingresa 10 valores")
+suma = 0
+for i in range (0,10):
+    num=int(input())
+    if num >0:
+        print("Numero positivo")
+        suma = suma + num
+    elif num < 0:
+        print("Numero negativo")
+        suma= suma - num
+    elif num == 0:
+        suma = 0
+print("El resultado total es de: ",suma)
