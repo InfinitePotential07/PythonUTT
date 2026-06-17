3c4c43a41b72e3ca275c7dff5ca41c8741b49c3f
InfinitePotential07 <ulisesteran2001@gmail.com>
2026-06-17 14:56:54 -0700

    Actualizacion en el ultima practica de funciones

--- Clase9_Funciones/funcionPromedio.py
+++ Clase9_Funciones/funcionPromedio.py
@@ -28,4 +28,5 @@ def numMay(num1,num2):
         return num1
     else:
         return num2
-print("El numero mas grande es ",numMay(6,9))
+mayor = numMay(num1=int(input("Ingresa el primer numero: ")), num2=int(input("Ingresa el segundo numero: ")))
+print("El numero mas grande es: ", mayor)
